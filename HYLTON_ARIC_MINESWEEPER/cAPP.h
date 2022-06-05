e10a3a767753f88670fb89157d145cd59550fa8a
Aric Hylton <Ahylton@student.fullsail.edu>
2022-06-05 12:34:27 -0500

    Add project files.

---
+++ HYLTON_ARIC_MINESWEEPER/cAPP.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "wx/wx.h"
+#include "cMain.h"
+
+class cAPP : public wxApp
+{
+public:
+	cAPP();
+	~cAPP();
+
+private:
+	cMain* m_frame1 = nullptr;
+
+public:
+	virtual bool OnInit();
+};
+
